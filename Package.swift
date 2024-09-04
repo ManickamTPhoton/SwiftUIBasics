36a2995dbc9ad4294135ebdc947bc45a6dc14c4d
ManickamTPhoton <manickam.thankgaraj@adusa.com>
2024-09-04 11:56:37 +0530

    second commit

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftUIBasics",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12),
+        .macOS(.v10_15),
         .tvOS(.v14),
         .watchOS(.v6)
     ],
--- Sources/SwiftUIBasics/SwiftUIBasics.swift
+++ Sources/SwiftUIBasics/SwiftUIBasics.swift
@@ -1,2 +1,71 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Combine
+
+class TextFieldObserver : ObservableObject {
+    @Published var debouncedText = ""
+    @Published var searchText = ""
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init() {
+        $searchText
+            .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] t in
+                self?.debouncedText = t
+            } )
+            .store(in: &subscriptions)
+    }
+}
+
+@available(macOS 11.0, *)
+public struct SerachView: View {
+    @StateObject var textObserver = TextFieldObserver()
+    
+    @State var customText = ""
+    
+    public init() {}
+    
+    public var body: some View {
+    
+        VStack {
+            Text("You entered: \(textObserver.debouncedText)")
+                .padding()
+            TextField("Enter Something", text: $textObserver.searchText)
+                .frame(height: 30)
+                .padding(.leading, 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.blue, lineWidth: 1)
+                )
+                .padding(.horizontal, 20)
+            Divider()
+            Text(customText)
+            TextFieldWithDebounce(debouncedText: $customText)
+        }
+    }
+}
+
+@available(macOS 11.0, *)
+struct TextFieldWithDebounce : View {
+    @Binding var debouncedText : String
+    @StateObject private var textObserver = TextFieldObserver()
+    
+    var body: some View {
+    
+        VStack {
+            TextField("Enter Something", text: $textObserver.searchText)
+                .frame(height: 30)
+                .padding(.leading, 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.blue, lineWidth: 1)
+                )
+                .padding(.horizontal, 20)
+        }.onReceive(textObserver.$debouncedText) { (val) in
+            debouncedText = val
+        }
+    }
+}
